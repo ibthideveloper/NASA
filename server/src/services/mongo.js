1c1121f89eb93fbd087acdb8ba3547fd488af3b4
ibthideveloper <99708892+ibthideveloper@users.noreply.github.com>
2023-08-27 07:55:46 +0530

    ** secured api key

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:Ibthikar@nasacluster.sxsy1ng.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("Monmgodb connectioon is ready");
